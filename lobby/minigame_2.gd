5c2b7826db9e1595eb14fab656bf7248369fcca2
real-chicken-master <aomkennedy@gmail.com>
2025-07-19 14:09:01 +1200

    got splitscreen working

--- lobby/minigame_2.gd
+++ lobby/minigame_2.gd
@@ -5,4 +5,4 @@ extends Area2D
 func _process(_delta):
 	for body in get_overlapping_bodies():
 		if (body.name == "player1" || "player2"):
-			get_tree().change_scene_to_file("res://Minigames/road_brawl.tscn")
+			Globals.change_scene(preload("res://Minigames/road_brawl.tscn").instantiate())
