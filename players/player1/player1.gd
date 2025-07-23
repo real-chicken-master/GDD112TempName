dd926ed8857dcc7f66c5392beea9c1e0b92d79bd
real-chicken-master <aomkennedy@gmail.com>
2025-07-23 12:17:34 +1200

    started to change move system

--- players/player1/player1.gd
+++ players/player1/player1.gd
@@ -7,16 +7,18 @@ var right = false
 var up = false
 var down = false
 
+var Xvelocity = 0
+var Yvelocity = 0
+
 
 func _process(delta):
 	if(left):
-		global_position.x -= SPEED * delta
+		Xvelocity -= SPEED
 	if(right):
-		global_position.x += SPEED * delta
+		Xvelocity = SPEED
 	if(up):
-		global_position.y -= SPEED * delta
-	if(down):
-		global_position.y += SPEED * delta
+		Yvelocity = -SPEED
+	move_and_collide(Vector2(Xvelocity,Yvelocity))
 
 
 func _input(event):
@@ -27,9 +29,9 @@ func _input(event):
 			if event.keycode == KEY_D:
 				right = true
 			if event.keycode == KEY_W:
-				up = true
+				Yvelocity -= SPEED
 			if event.keycode == KEY_S:
-				down = true
+				Yvelocity += SPEED
 		if(event.is_released()):
 			if event.keycode == KEY_A:
 				left = false
