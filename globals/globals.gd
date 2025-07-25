e9b1afd8f9e50683eeed61ccf72c2e7f7e5fa02a
real-chicken-master <aomkennedy@gmail.com>
2025-07-25 13:08:22 +1200

    Update globals.gd

--- globals/globals.gd
+++ globals/globals.gd
@@ -1,5 +1,12 @@
 extends Node
 
+@onready var players = get_tree().get_nodes_in_group("player")
+
+var movemode = "topdown":
+	set(mode):
+		movemode = mode
+		for player in players:
+			player.movemode = mode
 
 func change_scene(level):
 	var levelSwitcher = get_tree().get_first_node_in_group("levelSwitcher")
