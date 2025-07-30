9da12087e72520159c33d21d38ff4ca0c9271160
real-chicken-master <aomkennedy@gmail.com>
2025-07-31 09:37:02 +1200

    did more work on maze

--- Minigames/maze/maze.gd
+++ Minigames/maze/maze.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
-var mazeArray:Vector2
+var mazeArray: Array[Vector2]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	for camera in get_tree().get_nodes_in_group("camera"):
+		camera.zoom = Vector2(0.5,0.5)
+	for num in 32:
+		for num2 in 18:
+			mazeArray.push_back(Vector2(num,num2))
+	for num in mazeArray.size():
+		$TileMap.set_cell(0,mazeArray[num],0,Vector2i(0, 0))
 	
