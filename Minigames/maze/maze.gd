47081020ca39e60b483b8a54afe7300d5ed70b65
Blorftime <zeierba@whs.school.nz>
2025-08-01 14:25:16 +1200

    Revert "Merge branch 'master' of https://github.com/real-chicken-master/GDD112TempName"
    This reverts commit 8f732dfa539ef419b398885a67565c990626938b, reversing
    changes made to 2806bde48f9fc8840e6a386042cf2a2f94ae796d.

--- Minigames/maze/maze.gd
+++ Minigames/maze/maze.gd
@@ -1,23 +1,13 @@
 extends Node2D
 
-var mazeArray: Array[Array] = []
-const MAZE_WIDTH = 32
-const MAZE_HEIGHT = 18
- 
+var mazeArray: Array[Vector2]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	
 	for camera in get_tree().get_nodes_in_group("camera"):
 		camera.zoom = Vector2(0.6,0.6)
-		
-	#Array X
-	for numX in MAZE_WIDTH:
-		mazeArray.append([])
-		
-		for numY in MAZE_HEIGHT:
-			mazeArray[numX].append(true)
-		
-	for numX in MAZE_WIDTH:
-		for numY in MAZE_HEIGHT:
-			print(mazeArray[numX][numY])
+	for num in 32:
+		for num2 in 18:
+			mazeArray.push_back(Vector2(num,num2))
+	for num in mazeArray.size():
+		$TileMap.set_cell(0,mazeArray[num],0,Vector2i(0, 0))
+	
