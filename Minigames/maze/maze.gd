a03cb946a49f105f24b47dea4bd1972cbb899b69
real-chicken-master <aomkennedy@gmail.com>
2025-08-06 12:00:50 +1200

    Update maze.gd

--- Minigames/maze/maze.gd
+++ Minigames/maze/maze.gd
@@ -4,14 +4,10 @@ var mazeArray: Array[Array] = [] # true is collison false is no collision
 const MAZE_WIDTH = 32
 const MAZE_HEIGHT = 32
 var mapbuilt = false
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	
-	
 	for camera in get_tree().get_nodes_in_group("camera"):
 		camera.zoom = Vector2(0.6,0.6)
-		
-	#Array X
 	for numX in MAZE_WIDTH:
 		mazeArray.append([])
 		for numY in MAZE_HEIGHT:
@@ -20,30 +16,29 @@ func _ready():
 		var X = 1
 		var Y = 1
 		mazeArray[1][1] = false
-		for num in 100:
-			print(num)
+		while(canMove(X,Y)):
 			var moved = false
 			while (!moved):
 				var direction = randi_range(1,4)
 				match direction:
 					1:
 						if(Y-1 != 0):
-							if(mazeArray[X][Y-1] == true && mazeArray[X][Y-2]) != false:
+							if(mazeArray[X][Y-1] == true && mazeArray[X][Y-2]):
 								Y -= 1
 								moved = true
 					2:
 						if(Y+1 != MAZE_HEIGHT-1):
-							if(mazeArray[X][Y+1] == true && mazeArray[X][Y+2]) != false:
+							if(mazeArray[X][Y+1] == true && mazeArray[X][Y+2]):
 								Y += 1
 								moved = true
 					3:
 						if(X-1 != 0):
-							if(mazeArray[X-1][Y] == true && mazeArray[X-2][Y]) != false:
+							if(mazeArray[X-1][Y] == true && mazeArray[X-2][Y]):
 								X -= 1
 								moved = true
 					4:
-						if(X+1 != MAZE_HEIGHT-1):
-							if(mazeArray[X+1][Y] == true && mazeArray[X+2][Y]) != false:
+						if(X+1 != MAZE_WIDTH-1):
+							if(mazeArray[X+1][Y] == true && mazeArray[X+2][Y]):
 								X += 1
 								moved = true
 			mazeArray[X][Y] = false
@@ -54,3 +49,19 @@ func _ready():
 				$TileMap.set_cell(0,Vector2(numX,numY),0,Vector2(0,0))
 			else:
 				$TileMap.set_cell(0,Vector2(numX,numY),2,Vector2(0,0))
+
+func canMove(X,Y):
+	var Return = false
+	if(Y-1 != 0):
+		if(mazeArray[X][Y-1] == true && mazeArray[X][Y-2]):
+			Return = true
+	if(Y+1 != MAZE_HEIGHT-1):
+		if(mazeArray[X][Y+1] == true && mazeArray[X][Y+2]):
+			Return = true
+	if(X-1 != 0):
+		if(mazeArray[X-1][Y] == true && mazeArray[X-2][Y]):
+			Return = true
+	if(X+1 != MAZE_WIDTH-1):
+		if(mazeArray[X+1][Y] == true && mazeArray[X+2][Y]):
+			Return = true
+	return Return
