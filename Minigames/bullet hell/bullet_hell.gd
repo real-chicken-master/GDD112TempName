6707ce51576bbb099a3fd2cc5b0f8551c2736e8a
Blorftime <zeierba@whs.school.nz>
2025-08-06 12:17:42 +1200

    bullet hell code restarted

--- Minigames/bullet hell/bullet_hell.gd
+++ Minigames/bullet hell/bullet_hell.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+var bullets = 
 
 
 func _ready():
-	print(get_child_count())
+	var spawnpos = $"Bullet Spawns".get_child(randi()%$"Bullet Spawns".get_child_count()).global_position
+	print(spawnpos)
 
 
-func _process(delta):
+func _process(_delta):
 	pass
