5a22a8fee05c74003c1a660e608ad08a8c6d72fc
real-chicken-master <aomkennedy@gmail.com>
2025-08-08 21:12:36 +1200

    fixed the bug and started to add code that allows multiple paths to be generated

--- Minigames/maze/maze.gd
+++ Minigames/maze/maze.gd
@@ -17,14 +17,38 @@ func _ready():
 			maze_array[num_x].append(true)
 	#build maze
 	while(!map_built):
-		#create a x position
-		var X = 1
-		#create a y position
-		var Y = 1
 		#set the starting tile in the array
 		maze_array[1][1] = false
-		#while can move is true
-		while(can_move(X,Y)):
+		#this variable will be changed when no more paths are possible
+		var paths_possible = true
+		#build all paths
+		create_path(1,1)
+		while(paths_possible):
+			var paths_built = 0
+			for num_x in MAZE_WIDTH:
+				for num_y in MAZE_HEIGHT:
+					if(can_make_path(num_x,num_y)):
+						paths_built += 1
+						create_path(num_x,num_y)
+			print("test")
+			if(paths_built == 0):
+				paths_possible = false
+		map_built = true
+	#nake the tile map the same as the array
+	for num_x in MAZE_WIDTH:
+		for num_y in MAZE_HEIGHT:
+			if maze_array[num_x][num_y]:
+				$TileMap.set_cell(0,Vector2(num_x,num_y),0,Vector2(0,0))
+			else:
+				$TileMap.set_cell(0,Vector2(num_x,num_y),2,Vector2(0,0))
+
+func can_make_path(x,y):
+	var Return = false
+	return Return
+
+func create_path(x,y):
+	#while can move is true
+		while(can_move(x,y)):
 			#set moved to false
 			var moved = false
 			#until the path has been moved
@@ -34,52 +58,46 @@ func _ready():
 				#check if the path can go that direction
 				match direction:
 					1:#up
-						if(Y-1 != 0):
-							if(maze_array[X][Y-1] && maze_array[X][Y-2]):
-								if(tile_has_space(X,Y-1,direction)):
-									Y -= 1
+						if(y-1 != 0):
+							if(maze_array[x][y-1] && maze_array[x][y-2]):
+								if(tile_has_space(x,y-1,direction)):
+									y -= 1
 									moved = true
 					2:#down
-						if(Y+1 != MAZE_HEIGHT-1):
-							if(maze_array[X][Y+1] && maze_array[X][Y+2]):
-								if(tile_has_space(X,Y+1,direction)):
-									Y += 1
+						if(y+1 != MAZE_HEIGHT-1):
+							if(maze_array[x][y+1] && maze_array[x][y+2]):
+								if(tile_has_space(x,y+1,direction)):
+									y += 1
 									moved = true
 					3:#left
-						if(X-1 != 0):
-							if(maze_array[X-1][Y] && maze_array[X-2][Y]):
-								if(tile_has_space(X-1,Y,direction)):
-									X -= 1
+						if(x-1 != 0):
+							if(maze_array[x-1][y] && maze_array[x-2][y]):
+								if(tile_has_space(x-1,y,direction)):
+									x -= 1
 									moved = true
 					4:#right
-						if(X+1 != MAZE_WIDTH-1):
-							if(maze_array[X+1][Y] && maze_array[X+2][Y]):
-								if(tile_has_space(X+1,Y,direction)):
-									X += 1
+						if(x+1 != MAZE_WIDTH-1):
+							if(maze_array[x+1][y] && maze_array[x+2][y]):
+								if(tile_has_space(x+1,y,direction)):
+									x += 1
 									moved = true
 			#set that tile to path
-			maze_array[X][Y] = false
-		map_built = true
-	for num_x in MAZE_WIDTH:
-		for num_y in MAZE_HEIGHT:
-			if maze_array[num_x][num_y]:
-				$TileMap.set_cell(0,Vector2(num_x,num_y),0,Vector2(0,0))
-			else:
-				$TileMap.set_cell(0,Vector2(num_x,num_y),2,Vector2(0,0))
+			maze_array[x][y] = false
+		#when the tile cant be moved anymore set map built to true and end the loop
 
-func can_move(X,Y):
+func can_move(x,y,):
 	var Return = false
-	if(Y-1 != 0):
-		if(maze_array[X][Y-1] && maze_array[X][Y-2]):
+	if(y-1 != 0):
+		if(tile_has_space(x,y-1,1)):
 			Return = true
-	if(Y+1 != MAZE_HEIGHT-1):
-		if(maze_array[X][Y+1] && maze_array[X][Y+2]):
+	if(y+1 != MAZE_HEIGHT-1):
+		if(tile_has_space(x,y+1,2)):
 			Return = true
-	if(X-1 != 0):
-		if(maze_array[X-1][Y] && maze_array[X-2][Y]):
+	if(x-1 != 0):
+		if(tile_has_space(x-1,y,3)):
 			Return = true
-	if(X+1 != MAZE_WIDTH-1):
-		if(maze_array[X+1][Y] && maze_array[X+2][Y]):
+	if(x+1 != MAZE_WIDTH-1):
+		if(tile_has_space(x+1,y,4)):
 			Return = true
 	return Return
 
@@ -88,16 +106,20 @@ func tile_has_space(x,y,direction):
 	var Return = true
 	match direction:
 		1:#up
-			if!(maze_array[x][y-1] && maze_array[x-1][y] && maze_array[x+1][y]):
-				Return = false
+			if(y-1 != 0 && x+1 != MAZE_WIDTH-1 && x-1 != 0):
+				if!(maze_array[x][y-1] && maze_array[x-1][y] && maze_array[x+1][y]):
+					Return = false
 		2:#down
-			if!(maze_array[x][y+1] && maze_array[x-1][y] && maze_array[x+1][y]):
-				Return = false
+			if(y+1 != MAZE_HEIGHT-1 && x+1 != MAZE_WIDTH-1 && x-1 != 0):
+				if!(maze_array[x][y+1] && maze_array[x-1][y] && maze_array[x+1][y]):
+					Return = false
 		3:#left
-			if!(maze_array[x][y-1] && maze_array[x][y+1] && maze_array[x-1][y]):
-				Return = false
+			if(y+1 != MAZE_HEIGHT-1 && y+1 != MAZE_HEIGHT-1 && x-1 != 0):
+				if!(maze_array[x][y-1] && maze_array[x][y+1] && maze_array[x-1][y]):
+					Return = false
 		4:#right
-			if!(maze_array[x][y-1] && maze_array[x+1][y] && maze_array[x][y+1]):
-				Return = false
+			if(y+1 != MAZE_HEIGHT-1 && y+1 != MAZE_HEIGHT-1 && x+1 != MAZE_WIDTH-1):
+				if!(maze_array[x][y-1] && maze_array[x+1][y] && maze_array[x][y+1]):
+					Return = false
 	return Return
 
