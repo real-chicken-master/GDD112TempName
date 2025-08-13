2b745126380723a403f9f27a3bdf25082a534e93
real-chicken-master <aomkennedy@gmail.com>
2025-08-14 09:16:58 +1200

    update maze

--- Minigames/maze/maze.gd
+++ Minigames/maze/maze.gd
@@ -18,7 +18,7 @@ func _ready():
 		for num_y in MAZE_HEIGHT:
 			maze_array[num_x].append(true)
 	#build maze
-	while((!map_built) or tiles_attempted < 100):
+	while((!map_built)):
 		#set the starting tile in the array
 		maze_array[1][1] = false
 		#this variable will be changed when no more paths are possible
@@ -34,6 +34,7 @@ func _ready():
 						create_path(num_x,num_y)
 			if(paths_built == 0):
 				paths_possible = false
+		#whenno more paths can be built anymore set map built to true and end the loop
 		map_built = true
 	#nake the tile map the same as the array
 	for num_x in MAZE_WIDTH:
@@ -53,56 +54,73 @@ func create_path(x,y):
 	print("test")
 	#while can move is true
 	while(can_move(x,y)):
-		tiles_attempted += 1
-		print(tiles_attempted)
 		#set moved to false
 		var moved = false
 		#until the path has been moved
-		while (!moved):
+		while (!moved && tiles_attempted < 100):
+			tiles_attempted += 1
+			print(tiles_attempted)
 			#get a random direction
 			var direction = randi_range(1,4)
 			#check if the path can go that direction
 			match direction:
 				1:#up
+					#boundary 
 					if(y-1 > 0):
 						if(maze_array[x][y-1] && maze_array[x][y-2]):
-							if(tile_has_space(x,y-1,direction)):
+							#check if the tile has space
+							if(tile_has_space(x,y-1,1)):
 								y -= 1
 								moved = true
 				2:#down
+					#boundary
 					if(y+1 < MAZE_HEIGHT-1):
 						if(maze_array[x][y+1] && maze_array[x][y+2]):
-							if(tile_has_space(x,y+1,direction)):
+							#check if the tile has space
+							if(tile_has_space(x,y+1,2)):
 								y += 1
 								moved = true
 				3:#left
+					#boundary
 					if(x-1 > 0):
 						if(maze_array[x-1][y] && maze_array[x-2][y]):
-							if(tile_has_space(x-1,y,direction)):
+							#check if the tile has space
+							if(tile_has_space(x-1,y,3)):
 								x -= 1
-								moved = true	
+								moved = true
 				4:#right
+					#boundary
 					if(x+1 < MAZE_WIDTH-1):
 						if(maze_array[x+1][y] && maze_array[x+2][y]):
-							if(tile_has_space(x+1,y,direction)):
+							#check if the tile has space
+							if(tile_has_space(x+1,y,4)):
 								x += 1
 								moved = true
 		#set that tile to path
 		maze_array[x][y] = false
-	#when the tile cant be moved anymore set map built to true and end the loop
+		tiles_attempted = 0
+		
 
-func can_move(x,y,):
+func can_move(x,y):
 	var Return = false
+	#boundary
 	if(y-1 > 0):
+		#check if the tile has space
 		if(tile_has_space(x,y-1,1)):
 			Return = true
+	#boundary
 	if(y+1 < MAZE_HEIGHT-1):
+		#check if the tile has space
 		if(tile_has_space(x,y+1,2)):
 			Return = true
+	#boundary
 	if(x-1 > 0):
+		#check if the tile has space
 		if(tile_has_space(x-1,y,3)):
 			Return = true
+	#boundary
 	if(x+1 < MAZE_WIDTH-1):
+		#check if the tile has space
 		if(tile_has_space(x+1,y,4)):
 			Return = true
 	return Return
@@ -112,18 +130,22 @@ func tile_has_space(x,y,direction):
 	var Return = true
 	match direction:
 		1:#up
+			#boundary
 			if(y-1 > 0 && x+1 < MAZE_WIDTH-1 && x-1 > 0):
 				if!(maze_array[x][y-1] && maze_array[x-1][y] && maze_array[x+1][y]):
 					Return = false
 		2:#down
+			#boundary
 			if(y+1 < MAZE_HEIGHT-1 && x+1 < MAZE_WIDTH-1 && x-1 > 0):
 				if!(maze_array[x][y+1] && maze_array[x-1][y] && maze_array[x+1][y]):
 					Return = false
 		3:#left
+			#boundary
 			if(y+1 < MAZE_HEIGHT-1 && y+1 < MAZE_HEIGHT-1 && x-1 > 0):
 				if!(maze_array[x][y-1] && maze_array[x][y+1] && maze_array[x-1][y]):
 					Return = false
 		4:#right
+			#boundary
 			if(y+1 < MAZE_HEIGHT-1 && y+1 < MAZE_HEIGHT-1 && x+1 < MAZE_WIDTH-1):
 				if!(maze_array[x][y-1] && maze_array[x+1][y] && maze_array[x][y+1]):
 					Return = false
