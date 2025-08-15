fa06aa1b8ee343ab5efdb7ae9470c3a784b8d023
real-chicken-master <aomkennedy@gmail.com>
2025-08-15 12:28:12 +1200

    fixed bug

--- lobby/main_lobby.gd
+++ lobby/main_lobby.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for lights in get_tree().get_nodes_in_group("player_light"):
-		lights.energy = 1
+		lights.energy = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
