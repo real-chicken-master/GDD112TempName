5639fa1710a889778c8ea43794fe07f2733d88dc
Blorf <zeierba@whs.school.nz>
2025-08-15 15:20:11 +1200

    Bullet hell almost finished

--- Minigames/bullet hell/bullet.gd
+++ Minigames/bullet hell/bullet.gd
@@ -2,16 +2,16 @@ extends Area2D
 
 const speed = 250
 var player_array = []
-var target_pos
+var direction
 
 func _ready():
 	for players in get_tree().get_nodes_in_group("player"):
 		player_array.push_front(players)
-	target_pos = (player_array[randi_range(0,1)].global_position)
+	var target_pos = (player_array[randi_range(0,1)].global_position)
+	direction = (target_pos - global_position).normalized()
 
 
 func _process(delta):
-	var direction = (target_pos - global_position).normalized()
 	global_position += direction * speed * delta
 	for body in get_overlapping_bodies():
 		if (body.name == "res://players/player1/player1.tscn" || "res://players/player 2/player2.tscn"):
