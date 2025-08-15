ca430229ede513f65a4673d47c71617118a113ae
real-chicken-master <aomkennedy@gmail.com>
2025-08-15 14:35:53 +1200

    made it so when the player loads into the lobby  it resets their global positon

--- lobby/main_lobby.gd
+++ lobby/main_lobby.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
-
+var start_pos = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	for players in get_tree().get_nodes_in_group("player"):
+		players.global_position = start_pos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
