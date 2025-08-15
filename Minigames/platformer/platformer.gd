647d4693f6a21f9286453d9593ac5bc08fdba129
mr-monkeman124 <edwardrhcox@gmail.com>
2025-08-15 13:19:55 +1200

    platfromer hit coutter

--- Minigames/platformer/platformer.gd
+++ Minigames/platformer/platformer.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var hitcounter = 0
 
 func _ready():
 	for players in get_tree().get_nodes_in_group("player"):
@@ -10,11 +11,14 @@ func _ready():
 func _process(_delta):
 	pass
 
-
-func _on_body_entered(body):
-	print (body)
-
-
-
-
 #each time a spike is entered by a player a point is gained, who ever gets to the end with the least points wins.
+func _on_body_entered(body):
+	if (body.name == "player1hit"):
+		Globals.player1hit += 1
+	if (body.name == "player2hit"):
+		Globals.player2hit += 1
+
+
+
+
+
--- globals/globals.gd
+++ globals/globals.gd
@@ -1,12 +1,25 @@
 extends Node
 
+var player1hit = 0:
+	set(timesHit):
+		player1hit = timesHit
+		get_tree().get_first_node_in_group("p1hitcounter").text = str(timesHit)
+
+var player2hit = 0:
+	set(timesHit2):
+		player2hit = timesHit2
+		get_tree().get_first_node_in_group("p2hitcounter").text = timesHit2
+
 @onready var players = get_tree().get_nodes_in_group("player")
+
 
 var movemode = "topdown":
 	set(mode):
 		movemode = mode
 		for player in players:
 			player.movemode = mode
+
+var hit_counter = 0
 
 func change_scene(level):
 	var levelSwitcher = get_tree().get_first_node_in_group("levelSwitcher")
