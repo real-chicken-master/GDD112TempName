f907fe731578ddc1be173b1b13f41f8a93217f45
real-chicken-master <aomkennedy@gmail.com>
2025-08-15 14:53:40 +1200

    update main loby

--- lobby/main_lobby.gd
+++ lobby/main_lobby.gd
@@ -5,6 +5,8 @@ var start_pos = [Vector2(0,0),Vector2(200,0)]
 func _ready():
 	get_tree().get_first_node_in_group("player1").global_position = start_pos[0]
 	get_tree().get_first_node_in_group("player2").global_position = start_pos[1]
+	for player_lights in get_tree().get_nodes_in_group("player_light"):
+		player_lights.queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
