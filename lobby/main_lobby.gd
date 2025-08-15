2df8c0aa1a3304bfcb11676f991c37e888fda08e
real-chicken-master <aomkennedy@gmail.com>
2025-08-15 14:42:30 +1200

    fixed bug

--- lobby/main_lobby.gd
+++ lobby/main_lobby.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-var start_pos = Vector2(0,0)
+var start_pos = [Vector2(0,0),Vector2(200,0)]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for players in get_tree().get_nodes_in_group("player"):
-		players.global_position = start_pos
+	get_tree().get_first_node_in_group("player1").global_position = start_pos[0]
+	get_tree().get_first_node_in_group("player2").global_position = start_pos[1]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
