c0e48cebaffc45f75fedaff4b791aa95630d13c5
mr-monkeman124 <edwardrhcox@gmail.com>
2025-08-19 14:15:58 +1200

    pled wokr

--- Minigames/platformer/platformer_win.gd
+++ Minigames/platformer/platformer_win.gd
@@ -10,6 +10,14 @@ func _ready():
 func _process(delta):
 	pass
 
-
+var player_1_finished: bool = false
+var player_2_finished: bool = false
 func _on_body_entered(body):
-	pass # Replace with function body.
+	if body.name == "player2": 
+		player_2_finished = true
+	if body.name == "player1":
+		player_1_finished = true
+	
+	if player_1_finished and player_2_finished:
+		if Globals.player_1_hit >= Globals.player_2_hit: print("player2win!")
+		if Globals.player_2_hit <= Globals.player_1_hit: print("player1win!")
