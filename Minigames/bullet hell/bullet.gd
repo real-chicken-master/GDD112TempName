1404b31d8133231ccc92528e29cb0e687bd7e5ab
Blorf <zeierba@whs.school.nz>
2025-08-29 14:29:49 +1200

    Bullet Hell semi final update

--- Minigames/bullet hell/bullet.gd
+++ Minigames/bullet hell/bullet.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 const speed = 250
-var player_array = []
+var player_array = [] # putting the players into an array so that they can be randomly picked to target
 var direction
 
 @onready var p1 = get_tree().get_first_node_in_group("player1")
@@ -12,7 +12,7 @@ func _ready():
 		player_array.push_front(players)
 	var target_pos = (player_array[randi_range(0,1)].global_position)
 	direction = (target_pos - global_position).normalized()
-# selecting a player to go towards
+# selecting a player to go towards and setting the target direction to move in
 
 func _process(delta):
 	global_position += direction * speed * delta
@@ -24,10 +24,11 @@ func _on_despawntimer_timeout():
 
 func hit():
 	Globals.change_scene(preload("res://lobby/main_lobby.tscn").instantiate())
+# when bullet hits a player, it will bring them to the main lobby
 
 # checking if its the players that made contact with the bullets and giving points to whoever didnt hit the bullet and the movement code toward the player chosen.
 func _on_body_entered(body):
-	if (body == p1 || p2):
+	if (body == p1 or body == p2):
 		if (body == p1):
 			Globals.p2_Score += 1
 		if (body == p2):
