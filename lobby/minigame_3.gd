794bcadcc216412939fedea9917ee01ef7fb948d
mr-monkeman124 <edwardrhcox@gmail.com>
2025-09-04 09:24:17 +1200

    Randomeiser

--- lobby/minigame_3.gd
+++ lobby/minigame_3.gd
@@ -6,13 +6,14 @@ func _process(_delta):
 	for body in get_overlapping_bodies():
 		if (body.name == "player1" || "player2"):
 			var prize_number = randi_range(1,4)
-			if prize_number == 1:
-				$"../MeatPie".visible = true
-			if prize_number == 2:
-				$"../CinderBlock".visible = true
-			if prize_number == 3:
+			match prize_number:
+				1:
+					$"../MeatPie".visible = true
+				2:
+					$"../CinderBlock".visible = true
+				3:
 					$"../PaperTrophy".visible = true
-			if prize_number == 4:
+				4:
 					$"../Sock".visible = true
 
 			if Globals.p1_Score > Globals.p2_Score:
