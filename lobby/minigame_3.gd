23b10a0aac9e79cdce621efde69af0c6d524f741
mr-monkeman124 <edwardrhcox@gmail.com>
2025-09-03 12:19:20 +1200

    full game win is now possible i just need to make it so only one prize appears

--- lobby/minigame_3.gd
+++ lobby/minigame_3.gd
@@ -5,4 +5,19 @@ extends Area2D
 func _process(_delta):
 	for body in get_overlapping_bodies():
 		if (body.name == "player1" || "player2"):
-			Globals.change_scene(preload("res://Minigames/maze/maze.tscn").instantiate())
+			var prize_number = randi_range(1,4)
+			if prize_number == 1:
+				$"../MeatPie".visible = true
+			if prize_number == 2:
+				$"../CinderBlock".visible = true
+			if prize_number == 3:
+					$"../PaperTrophy".visible = true
+			if prize_number == 4:
+					$"../Sock".visible = true
+
+			if Globals.p1_Score > Globals.p2_Score:
+				$"../P1FullWin".visible = true
+			if Globals.p1_Score < Globals.p2_Score:
+				$"../P2FullWin".visible = true
+
+
