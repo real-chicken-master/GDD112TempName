d37a490bb07dd9a1f9d0e3b6052c41dda0138227
Blorf <zeierba@whs.school.nz>
2025-09-05 14:51:04 +1200

    title screen decorated

--- titleScreen/title_screen.gd
+++ titleScreen/title_screen.gd
@@ -1,7 +1,9 @@
 extends Control
 
-
-
+func _process(_delta):
+	$BackgroundArt.rotation_degrees += 2.5
+	$ArcadeWall.rotation_degrees += 2.5
+	pass
 
 func _on_button_button_down():
 	get_tree().change_scene_to_file("res://UI/splitscreen.tscn")
