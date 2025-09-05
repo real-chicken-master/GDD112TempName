f35ac1d01a1bac7f9ebf2cb4049952ba653e1cb3
real-chicken-master <aomkennedy@gmail.com>
2025-09-05 15:17:57 +1200

    did stuff

--- titleScreen/title_screen.gd
+++ titleScreen/title_screen.gd
@@ -1,9 +1,17 @@
 extends Control
 
+var rotate =  randf_range(-1,1)
+var button_Rotate = randf_range(-1,1)
 func _process(_delta):
-	$BackgroundArt.rotation_degrees += 2.5
-	$ArcadeWall.rotation_degrees += 2.5
-	pass
+	$BackgroundArt.rotation_degrees += rotate
+	$ArcadeWall.rotation_degrees -= rotate
+	$Button.rotation_degrees += button_Rotate
 
 func _on_button_button_down():
 	get_tree().change_scene_to_file("res://UI/splitscreen.tscn")
+
+
+func _on_timer_timeout():
+	rotate =  randf_range(-1,1)
+	button_Rotate = randf_range(-1,1)
+	$Timer.start()
